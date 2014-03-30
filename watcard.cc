16f1cbdbf773dfb1d0c2feb92390e4d38725f0ab
Chen Zeyu <zeyufly@gmail.com>
2014-03-30 19:41:29 -0400

    added watcard.cc

---
+++ watcard.cc
@@ -0,0 +1,20 @@
+// CS343 - Concurrent and Parallel Programming
+// Winter, 2014
+// Assignment 6
+#include "watcard.h"
+
+WATCard::WATCard(){
+    balance = 0;
+}
+
+void WATCard::deposit(unsigned int amount){
+    balance += amount;
+}
+
+void WATCard::withdraw(unsigned int amount){
+    balance -= amount;
+}
+
+unsigned int WATCard::getBalance(unsigned int amount){
+    return balance;
+}
