f215b2be17d7188889181dd50f93f8d43ed14d40
Chen Zeyu <zeyufly@gmail.com>
2014-03-30 17:39:37 -0400

    added printer.cc, updated readme

--- README.md
+++ README.md
@@ -13,3 +13,12 @@ Andrew TODO
 BottlingPlant, NameServer, Truck, VendingMachine all compile but haven't been tested.
 
 **Note:** Call `VendingMachine::NUM_FLAVOURS` to get the number of flavours in the vending machine.
+
+Chen TODO
+------------
+* WatCard
+* WatCardOffice
+* Parent
+* Bank
+
+All haven't tested.
--- printer.h
+++ printer.h
@@ -1,14 +1,32 @@
 #ifndef PRINTER_H
 #define PRINTER_H
 _Monitor Printer {
-  public:
-    enum Kind { Parent, WATCardOffice, NameServer, Truck, BottlingPlant, Student, Vending, Courier };
-    Printer( unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers );
-    void print( Kind kind, char state );
-    void print( Kind kind, char state, int value1 );
-    void print( Kind kind, char state, int value1, int value2 );
-    void print( Kind kind, unsigned int lid, char state );
-    void print( Kind kind, unsigned int lid, char state, int value1 );
-    void print( Kind kind, unsigned int lid, char state, int value1, int value2 );
+    private:
+        unsigned int numStudents;
+        unsigned int numVendingMachines;
+        unsigned int numCouriers;
+        unsigned int numColumns;
+        struct printingItem{
+            unsigned int value1;
+            unsigned int value2;
+            unsigned int id;
+            char state;
+            bool hasValues;
+        }
+        printingItem *items;
+        _Mutex void Printer::printMaster( unsigned int id, char state, int value1, int value2);
+        void flush();
+        unsigned int calculateID(Kind k);
+
+    public:
+        enum Kind { Parent, WATCardOffice, NameServer, Truck, BottlingPlant, Student, Vending, Courier };
+        Printer( unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers );
+        void print( Kind kind, char state );
+        void print( Kind kind, char state, int value1 );
+        void print( Kind kind, char state, int value1, int value2 );
+        void print( Kind kind, unsigned int lid, char state );
+        void print( Kind kind, unsigned int lid, char state, int value1 );
+        void print( Kind kind, unsigned int lid, char state, int value1, int value2 );
+        ~Printer();
 };
 #endif
